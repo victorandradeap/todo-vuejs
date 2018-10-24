811830886a4fec75128235def1080cccd6be5eaa
victorandradeap <victorandrade@grupoteknisa.com>
2018-10-24 14:05:53 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todo List VueJs
+## A Todo List Vue.js project to study.
+Create with Vue Cli.
---
+++ src/components/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Todo</h1>
+        <todo-form/>
+        <todo-list/>
+    </div>
+</template>
+<script>
+import TodoForm from '@/views/TodoForm';
+import TodoList from '@/views/TodoList';
+
+export default {
+    name: 'Todo',
+    components: {
+        TodoList,
+        TodoForm
+    }
+}
+</script>
+
